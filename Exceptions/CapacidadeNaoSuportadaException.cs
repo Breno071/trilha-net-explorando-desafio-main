2d18c74fd5717154510a63ed001f224cf6f059bb
Breno Narde Silva <brenonarde@gmail.com>
2023-09-22 17:15:47 -0300

    Add project files.

---
+++ Exceptions/CapacidadeNaoSuportadaException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioProjetoHospedagem.Exceptions
+{
+
+	[Serializable]
+	public class CapacidadeNaoSuportadaException : Exception
+	{
+		public CapacidadeNaoSuportadaException() { }
+		public CapacidadeNaoSuportadaException(string message) : base(message) { }
+		public CapacidadeNaoSuportadaException(string message, Exception inner) : base(message, inner) { }
+		protected CapacidadeNaoSuportadaException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+}
---
+++ Models/Reserva.cs
@@ -0,0 +1,53 @@
+using DesafioProjetoHospedagem.Exceptions;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new CapacidadeNaoSuportadaException("Capacidade da suite não suportada");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor -= valor * 0.10M;
+            }
+
+            return valor;
+        }
+    }
+}
